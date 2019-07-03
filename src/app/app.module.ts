e3a05777926f7c60cda06284aba63af0ebac12f0
panicker <lalit.panicker@leadics.com>
2019-07-03 13:35:29 +0530

    Skeleton added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoItemsComponent } from './components/todos/todoComponents/todo-items/todo-items.component';
+import { AddTodoComponent } from './components/todos/todoComponents/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    TodoItemsComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
